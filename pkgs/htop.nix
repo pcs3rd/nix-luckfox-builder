70aebbfd731e08fe5b5c38824c6caae2bbff9258
pcs3rd <rhean1620@gmail.com>
2026-04-20 17:58:31 -0400

    dunno

--- pkgs/htop.nix
+++ pkgs/htop.nix
@@ -1,33 +1,33 @@
 # Example custom package: a small static C utility that prints system info.
 
 
-{ lib, stdenv, fetchFromGitHub, ncurses, pkg-config, autoreconfHook}:{
+{ lib, stdenv, fetchFromGitHub, ncurses, pkg-config, autoreconfHook }:
 
-  pkgs.pkgsStatic.stdenv.mkDerivation {
-    pname   = "htop";
-    version = "3.5.0";
+pkgs.pkgsStatic.stdenv.mkDerivation {
+  pname   = "htop";
+  version = "3.5.0";
 
-    # Single-file project — point src directly at the .c file.
-    src = fetchFromGitHub {
-      "owner": "htop-dev",
-      "repo": "htop",
-      "rev": "dd9d7b100faa8ae57ec20be32d6353952b15eeec",
-      "hash": "sha256-gydXIExIdsTbCQnyqlMf9h77hzPqihDr5FLw1pzSiWg="
-    };
-
-    nativeBuildInputs = [
-      autoreconfHook
-      pkg-config
-    ];
-
-    buildInputs = [
-      ncurses
-    ];
-
-    configureFlags = [
-      "--enable-unicode"
-    ];
-    meta.description = "htop";
+  # Single-file project — point src directly at the .c file.
+  src = fetchFromGitHub {
+    "owner": "htop-dev",
+    "repo": "htop",
+    "rev": "dd9d7b100faa8ae57ec20be32d6353952b15eeec",
+    "hash": "sha256-gydXIExIdsTbCQnyqlMf9h77hzPqihDr5FLw1pzSiWg="
   };
-}
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  buildInputs = [
+    ncurses
+  ];
+
+  configureFlags = [
+    "--enable-unicode"
+  ];
+  meta.description = "htop";
+};
+
 
